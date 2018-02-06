2090a97f1f417a47f1d0a211d6e2d2e6a55f3aad
testcasing <nayanikanaik94@gmail.com>
2018-02-07 01:07:29 +0530

    image resizer working

--- index.js
+++ index.js
@@ -1,54 +1,77 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
+const imgURL = "https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg";
+var Jimp = require("jimp");
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/api', function api(req, res) {
-  res.json({
-    description: 'My API. Please authenticate!'
-  });
+    res.json({
+        description: 'My API. Please authenticate!'
+    });
 });
 
 app.post('/api/login', function(req, res) {
 
-// insert code here to actually authenticate, or fake it
-const user = { id: 3 };
+    // insert code here to actually authenticate, or fake it
+    const user = {
+        id: 3
+    };
 
-// then return a token, secret key should be an env variable
-const token = jwt.sign({ user: user.id }, 'secret_key_goes_here');
-res.json({
-message: 'Authenticated! Use this token in the "Authorization" header',
-token: token
-});
+    // then return a token, secret key should be an env variable
+    const token = jwt.sign({
+        user: user.id
+    }, 'secret_key_goes_here');
+    res.json({
+        message: 'Authenticated! Use this token in the "Authorization" header',
+        token: token
+    });
 });
 
 
 app.get('/api/protected', ensureToken, function(req, res) {
-  jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      res.json({
-        description: 'Protected information. Congrats!'
-      });
-    }
-  });
+    jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                description: 'Protected information. Congrats!'
+            });
+        }
+    });
+});
+
+
+app.get('/api/resizeimage/:imgURL', ensureToken, function(req, res) {
+    jwt.verify(req.token, 'secret_key_goes_here', function(err, data) {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            Jimp.read(req.param('imgURL'), function(err, img) {
+                if (err) throw err;
+                img.resize(50, 50).getBase64(Jimp.AUTO, function(e, img64) {
+                    if (e) throw e
+                    res.send('<img src="' + img64 + '">')
+                });
+            });
+        }
+    });
 });
 
 function ensureToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
-  if (typeof bearerHeader !== 'undefined') {
-    const bearer = bearerHeader.split(" ");
-    const bearerToken = bearer[1];
-    req.token = bearerToken;
-    next();
-  } else {
-    res.sendStatus(403);
-  }
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
 }
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(3000, function() {
+    console.log('App listening on port 3000!');
 });
